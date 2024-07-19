becaef778363f27eb354ee4c9c49fecc7ca55c29
04gab <106406199+04gab@users.noreply.github.com>
2024-07-19 13:08:02 -0700

    update notes 12

--- pytorch/pytorch notes/pytorch_notes12.py
+++ pytorch/pytorch notes/pytorch_notes12.py
@@ -1,5 +1,6 @@
 #%%
 import torch 
+from torch import nn
 import pandas as pd
 import numpy as np
 import sklearn
@@ -58,4 +59,75 @@ X_train, X_test, y_train, y_test = train_test_split(X,
                                                     random_state=42) 
 # %%
 print(len(X_train), len(X_test), len(y_train), len(y_test))
+# %%
+#building model (classify blue and red dots)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(device)
+# %%
+class CircleModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features=2, #takes in 2 features and upscales to 5 features
+                                 out_features=5)
+        self.layer_2 = nn.Linear(in_features=5, #takes in 5 features from previous layer and outputs 1 feature (same shape as y)
+                                 out_features=1)
+
+    def forward(self, x):
+        return self.layer_2(self.layer_1(x)) #x -> layer_1 -> layer_2 -> output
+    
+#instantiate an instance of model and send to target device
+model_0 = CircleModel().to(device)
+next(model_0.parameters()).device
+
+# %%
+#replicated model
+model_0 = nn.Sequential(
+    nn.Linear(in_features=2, out_features=5),
+    nn.Linear(in_features=5, out_features=1)
+).to(device)
+model_0
+# %%
+model_0.state_dict()
+# %%
+with torch.inference_mode():
+    untrained_preds = model_0(X_test.to(device))
+
+print(f"Length of predictions: {len(untrained_preds)}, Shape: {untrained_preds.shape}")
+print(f"Length of test samples: {len(X_test)}, Shape: {X_test.shape}")
+print(f"\nFirst 10 predictions: \n {untrained_preds[:10]}")
+print(f"\nFirst 10 Labels: \n{y_test[:10]}")
+# %%
+#loss_fn = nn.BCELoss() #requires input to have gone through the sigmoid activation function prior to input to BCELoss
+loss_fn = nn.BCEWithLogitsLoss() #sigmoid activation function built in
+
+optimizer = torch.optim.SGD(params=model_0.parameters(),
+                            lr=0.1)
+#calculate accuracy - out of 100 examples, what # does our model get right?
+
+def accuracy(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct/len(y_pred)) *100
+    return acc
+#%%
+#model's output are raw "logits" -> convert raw logits into prediction probabilities by passing them to activation function 
+
+#view first 5 outputs of forawrd pass on the test data
+with torch.inference_mode():
+    y_logits = model_0(X_test.to(device))[:5]
+print(y_logits)
+# %%
+#use sigmoid activation function on our model logits
+y_pred_probs = torch.sigmoid(y_logits)
+print(y_pred_probs)
+# %%
+torch.round(y_pred_probs)
+#%%
+y_preds = torch.round(y_pred_probs)
+#logits -> pred probs -> pred labels
+y_pred_labels = torch.round(torch.sigmoid(model_0(X_test.to(device))[:5]))
+print(torch.eq(y_preds.squeeze(), y_pred_labels.squeeze()))
+y_preds.squeeze()
+# %%
+y_test[:5]
 # %%
