fb7e4ed1b63df2bea37b4b86ad8fdf8af2b5cd3a
04gab <106406199+04gab@users.noreply.github.com>
2024-07-20 21:59:08 -0700

    notes 17 buggy

--- pytorch/pytorch notes/pytorch_notes17.py
+++ pytorch/pytorch notes/pytorch_notes17.py
@@ -1,8 +1,66 @@
+#%%
 import torch 
+from torch import nn
 import pandas as pd
 import numpy as np
-import timeit
 import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
 print(torch.__version__)
-# pytorch and numpy
+# multiclassification
 # ----------------------------------------------------------------------------------------------------------
+#%%
+NUM_CLASSES = 4
+NUM_FEATURES = 2
+RANDOM_SEED = 42
+
+X_blob, y_blob = make_blobs(n_samples=1000,
+                            n_features=NUM_FEATURES,
+                            centers=NUM_CLASSES,
+                            cluster_std=1.5, #gives the cluster a little shake up
+                            random_state=RANDOM_SEED)
+
+X_blob = torch.from_numpy(X_blob).type(torch.float)
+y_blob = torch.from_numpy(y_blob).type(torch.float)
+
+X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(X_blob,
+                                                                        y_blob,
+                                                                        test_size=0.2,
+                                                                        random_state=RANDOM_SEED)
+#%%
+#plot data
+plt.figure(figsize=(10, 7))
+plt.scatter(X_blob[:, 0], X_blob[:, 1], c=y_blob, cmap=plt.cm.RdYlBu)
+# %%
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# %%
+class BlobModel(nn.Module):
+  def __init__(self, input_features, output_features, hidden_units=8):
+    super().__init__()
+    self.linear_layer_stack = nn.Sequential(
+        nn.Linear(in_features=input_features, out_features=hidden_units),
+        # nn.ReLU(),
+        nn.Linear(in_features=hidden_units, out_features=hidden_units),
+        # nn.ReLU(),
+        nn.Linear(in_features=hidden_units, out_features=output_features)
+    )
+
+  def forward(self, x):
+    return self.linear_layer_stack(x)
+#%%
+model_4 = BlobModel(input_features=2,
+                    output_features=4,
+                    hidden_units=8)
+
+# %%
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model_4.parameters(),
+                            lr=0.1)
+#%%
+model_4.eval()
+with torch.inference_mode():
+  y_logits = model_4(X_blob_test)
+
+y_logits[:10]
+# %%
