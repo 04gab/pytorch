bb7cf4927754e95a5c45292b32f9b1df10d5ace1
04gab <106406199+04gab@users.noreply.github.com>
2024-07-21 00:35:10 -0700

    bug

--- pytorch/pytorch notes/pytorch_notes17.py
+++ pytorch/pytorch notes/pytorch_notes17.py
@@ -13,6 +13,7 @@ print(torch.__version__)
 NUM_CLASSES = 4
 NUM_FEATURES = 2
 RANDOM_SEED = 42
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 X_blob, y_blob = make_blobs(n_samples=1000,
                             n_features=NUM_FEATURES,
@@ -21,18 +22,18 @@ X_blob, y_blob = make_blobs(n_samples=1000,
                             random_state=RANDOM_SEED)
 
 X_blob = torch.from_numpy(X_blob).type(torch.float)
-y_blob = torch.from_numpy(y_blob).type(torch.float)
+y_blob = torch.from_numpy(y_blob).type(torch.LongTensor)
 
 X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(X_blob,
                                                                         y_blob,
                                                                         test_size=0.2,
                                                                         random_state=RANDOM_SEED)
 #%%
+
+#%%
 #plot data
 plt.figure(figsize=(10, 7))
 plt.scatter(X_blob[:, 0], X_blob[:, 1], c=y_blob, cmap=plt.cm.RdYlBu)
-# %%
-device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # %%
 class BlobModel(nn.Module):
@@ -51,16 +52,76 @@ class BlobModel(nn.Module):
 #%%
 model_4 = BlobModel(input_features=2,
                     output_features=4,
-                    hidden_units=8)
+                    hidden_units=8).to(device)
 
 # %%
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(params=model_4.parameters(),
                             lr=0.1)
 #%%
+X_blob_test.to(device)
+model_4.state_dict()
+#%%
 model_4.eval()
 with torch.inference_mode():
   y_logits = model_4(X_blob_test)
 
-y_logits[:10]
+# %%
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct/len(y_pred)) *100
+    return acc
+
+#%%
+y_pred_probs = torch.softmax(y_logits, dim=1)
+print(y_logits[:5])
+print(y_pred_probs[:5])
+# %%
+y_preds = torch.argmax(y_pred_probs, dim=1)
+
+# %%
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+epochs = 100
+X_blob_train, y_blob_train = X_blob_train.to(device), y_blob_train.to(device)
+X_blob_test, y_blob_test = X_blob_test.to(device), y_blob_test.to(device)
+
+#%%
+y_blob_train = y_blob_train.type(torch.long)
+y_blob_test = y_blob_test.type(torch.long)
+#%%
+# Put data to the target device
+X_blob_train, y_blob_train = X_blob_train.to(device), y_blob_train.to(device)
+X_blob_test, y_blob_test = X_blob_test.to(device), y_blob_test.to(device)
+
+# Loop through data
+for epoch in range(epochs):
+  ### Training 
+  model_4.train()
+
+  y_logits = model_4(X_blob_train)
+  y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+
+  
+  loss = loss_fn(y_logits, y_blob_train)
+  acc = accuracy_fn(y_true=y_blob_train,
+                    y_pred=y_pred)
+  
+  optimizer.zero_grad()
+  loss.backward()
+  optimizer.step()
+
+  ### Testing
+  model_4.eval()
+  with torch.inference_mode():
+    test_logits = model_4(X_blob_test)
+    test_preds = torch.softmax(test_logits, dim=1).argmax(dim=1)
+    
+    test_loss = loss_fn(test_logits, y_blob_test)
+    test_acc = accuracy_fn(y_true=y_blob_test,
+                           y_pred=test_preds)
+  
+  if epoch % 10 == 0:
+    print(f"Epoch: {epoch} | Loss: {loss:.4f}, Acc: {acc:.2f}% | Test loss: {test_loss:.4f}, Test acc: {test_acc:.2f}%")
 # %%
